3b5e56baf4ea2859dc21632b9ad8499c165d0266
Fanushhh <fgabriel.mihailescu@gmail.com>
2023-05-24 08:12:23 +0300

    6. Styled components

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,30 +3,32 @@ import { styled } from "styled-components";
 export default function AboutSection() {
   return (
     <About className="description">
-      <div className="title">
-        <div className="hide">
-          <h2>I work to make</h2>
-        </div>
-        <div className="hide">
-          <h2>
-            your <span className="highlight">dream website</span> come
-          </h2>
-        </div>
-        <div className="hide">
-          <h2>true.</h2>
+      <Description>
+        <div className="title">
+          <Hide className="hide">
+            <h2>I work to make</h2>
+          </Hide>
+          <Hide>
+            <h2>
+              your <span className="highlight">dream website</span>
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>come true.</h2>
+          </Hide>
         </div>
         <p>
           Contact me for any web development task you want to tackle or if you
           just wanna talk.
         </p>
         <button>Contact me</button>
-      </div>
-      <div className="profile-image">
+      </Description>
+      <Image className="profile-image">
         <img
           src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
           alt="about me image"
         />
-      </div>
+      </Image>
     </About>
   );
 }
@@ -38,9 +40,26 @@ const About = styled.div`
   justify-content: space-between;
   padding: 5rem 10rem;
   gap: 2rem;
-  .profile-image img {
-    max-width: 400px;
-    width: 100%;
-    display: block;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
   }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
 `;
--- src/components/globalStyle.js
+++ src/components/globalStyle.js
@@ -16,13 +16,40 @@ button{
     font-size:1.1rem;
     color:white;
     cursor:pointer;
-    border:3px solid green;
+    border:3px solid #23d997;
     background:transparent;
     padding:1rem 2rem;
     transition: all 250ms ease;
     &:hover{
-        background: green;
+        background: #23d997;
     }
+}
+
+h2{
+    font-size:4rem;
+    font-weight:lighter;
+
+}
+h3{
+    color:white;
+
+}
+h4{
+    font-weight:bold;
+    
+}
+span{
+    font-weight:bold;
+    color:#23d997;
+}
+p{
+    padding:3rem 0;
+    color:#ccc;
+    font-size:1.4rem;
+    line-height:150%;
+}
+a{
+    font-size:1.1rem;
 }
 
 `;
